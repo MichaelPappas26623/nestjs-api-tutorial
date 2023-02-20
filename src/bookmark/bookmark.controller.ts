4be028033f3ef2d8c623fb45b808fb7b238df192
Michael Pappas <michael.pappas@oohmedia.com.au>
2023-02-20 13:53:30 +1100

    creating the bookmarks logic

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -3,16 +3,18 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { GetUser } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 import { BookmarkService } from './bookmark.service';
-import { CreateBookmarkDto } from './dto';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('bookmarks')
@@ -20,23 +22,41 @@ export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
 
   @Get()
-  getBookmarks(@GetUser('id') userId: number) {}
+  getBookmarks(@GetUser('id') userId: number) {
+    return this.bookmarkService.getBookmarks(userId);
+  }
 
   @Get(':id')
   getBookmarkById(
     @GetUser('id') userId: number,
     @Param('id', ParseIntPipe) bookmarkId: number,
-  ) {}
+  ) {
+    return this.bookmarkService.getBookmarkById(userId, bookmarkId);
+  }
 
   @Post()
   createBookmark(
     @GetUser('id') userId: number,
     @Body() dto: CreateBookmarkDto,
-  ) {}
+  ) {
+    return this.bookmarkService.createBookmark(userId, dto);
+  }
 
-  @Patch('id')
-  editBookmarkById(@GetUser('id') userId: number) {}
+  @Patch(':id')
+  editBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+    @Body() dto: EditBookmarkDto,
+  ) {
+    return this.bookmarkService.editBookmarkById(userId, bookmarkId, dto);
+  }
 
-  @Delete('id')
-  deleteBookmarkById(@GetUser('id') userId: number) {}
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.deleteBookmarkById(userId, bookmarkId);
+  }
 }
