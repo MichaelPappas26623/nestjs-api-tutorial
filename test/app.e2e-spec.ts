f6e4e08d1457f7129c6214ca933e28eef3f225ec
Michael Pappas <michael.pappas@oohmedia.com.au>
2023-02-20 14:18:18 +1100

    typos

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -36,7 +36,7 @@ describe('App e2e', () => {
 
   describe('Auth', () => {
     const dto: AuthDto = {
-      email: 'vlad@gmail.com',
+      email: 'mike@gmail.com',
       password: '123',
     };
     describe('Signup', () => {
@@ -119,8 +119,8 @@ describe('App e2e', () => {
     describe('Edit user', () => {
       it('should edit user', () => {
         const dto: EditUserDto = {
-          firstName: 'Vladimir',
-          email: 'vlad@codewithvlad.com',
+          firstName: 'Michael',
+          email: 'mike@codewithmike.com',
         };
         return pactum
           .spec()
